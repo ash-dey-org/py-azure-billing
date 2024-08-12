8cbe4c604763c161ee7c1318b95175fe9f142650
Ash Dey <ashabc [at] hotmail.com>
2024-08-12 11:03:48 +1000

    chaged function trigger time to UTC 19:45 i.e. AEST 5:45

--- function_app.py
+++ function_app.py
@@ -1,8 +1,5 @@
 # to do
-# run function app 2nd of every month 0 45 5 2 * *
-# include email to Ashv and Prem
-# create a logic app to restart function every month
-# set run on restart to false
+# include Ashv and Prem in email DL
 
 # pip install requests
 # pip install azure-identity
@@ -148,9 +145,9 @@ query = {
 app = func.FunctionApp()
 
 @app.function_name(name="MonthlyBillingReport")
-@app.schedule(schedule="0 45 19 * * *",
+@app.schedule(schedule="0 45 19 3 * *",
               arg_name="MonthlyBillingReport",
-              run_on_startup=True)
+              run_on_startup=False)
 def main(MonthlyBillingReport: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.now(datetime.timezone.utc).isoformat()
     if MonthlyBillingReport.past_due:
